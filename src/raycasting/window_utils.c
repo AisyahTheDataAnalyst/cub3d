4a28a12907e648ea269b22eeb48cbe682484bea6
Yohen Sheraun <ysheraun20@yahoo.com>
2025-10-19 22:25:34 +0800

    Added utility file to create windows

--- src/raycasting/window_utils.c
+++ src/raycasting/window_utils.c
@@ -1,20 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   raycasting.c                                       :+:      :+:    :+:   */
+/*   window_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/19 21:44:35 by yosherau          #+#    #+#             */
-/*   Updated: 2025/10/19 22:11:48 by yosherau         ###   ########.fr       */
+/*   Created: 2025/10/19 21:52:57 by yosherau          #+#    #+#             */
+/*   Updated: 2025/10/19 21:59:35 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycasting.h"
 
-void	raycasting(t_player *player, t_map *map, t_raycast *raycast)
+# define WINDOW_WIDTH 854
+# define WINDOW_HEIGHT 480
+
+void	create_window(t_raycast *raycast)
 {
-	(void)player;
-	(void)map;
-	create_window(raycast);
+	raycast->mlx_connection = mlx_init();
+	raycast->mlx_window = mlx_new_window(raycast->mlx_connection,
+		WINDOW_WIDTH, WINDOW_HEIGHT, "cub3d");
 }
