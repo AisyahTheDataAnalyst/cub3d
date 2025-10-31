d74bbcdd23830e163a6c55cc98e90a652e6514fd
Yohen Sheraun <ysheraun20@yahoo.com>
2025-10-31 15:21:13 +0800

    feat: implemented rendering of walls via lode's graphics tutorial

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,24 +6,35 @@
 /*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 21:44:35 by yosherau          #+#    #+#             */
-/*   Updated: 2025/10/24 23:15:20 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/10/31 15:18:37 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <math.h>
 #include "raycasting.h"
+#include "cub3d.h"
+#include <math.h>
 
-void	raycasting(t_player *player, t_map *map, t_raycast *raycast)
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	create_window(raycast);
-	int	column = -1;
-	while (++column < WINDOW_WIDTH)
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	raycasting(t_game *game)
+{
+	int	x;
+
+	x = -1;
+	while (++x < WINDOW_WIDTH)
 	{
-		double	cameraX = 2 * column / (double)WINDOW_WIDTH - 1;
-		double	rayDirX = player->dir_x + player->plane_x * cameraX;
-		double	rayDirY = player->dir_y + player->plane_y * cameraX;
-		int	mapX = (int)player->x_pos;
-		int	mapY = (int)player->y_pos;
+		double	cameraX = 2 * x / (double)WINDOW_WIDTH - 1;
+		double	rayDirX = cameraX * game->player.plane_x + game->player.dir_x;
+		double	rayDirY = cameraX * game->player.plane_y + game->player.dir_y;
+
+		int	mapX = (int)game->player.x_pos;
+		int	mapY = (int)game->player.y_pos;
 
 		double	sideDistX;
 		double	sideDistY;
@@ -31,52 +42,74 @@ void	raycasting(t_player *player, t_map *map, t_raycast *raycast)
 		double	deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
 		double	deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
 
+		double	perpWallDist;
+
 		int	stepX;
 		int	stepY;
-		bool	wallHit = false;
-		// int	side;
+
+		int	hit = 0;
+		int	side;
+
 		if (rayDirX < 0)
 		{
 			stepX = -1;
-			sideDistX = (player->x_pos - mapX) * deltaDistX;
+			sideDistX = (game->player.x_pos - mapX) * deltaDistX;
 		}
 		else
 		{
 			stepX = 1;
-			sideDistX = (mapX + 1.0 - player->x_pos) * deltaDistX;
+			sideDistX = (mapX + 1.0 - game->player.x_pos) * deltaDistX;
 		}
-		if (rayDirY > 0)
+		if (rayDirY < 0)
 		{
 			stepY = -1;
-			sideDistY = (player->y_pos - mapY) * deltaDistY;
+			sideDistY = (game->player.y_pos - mapY) * deltaDistY;
 		}
 		else
 		{
 			stepY = 1;
-			sideDistY = (mapY + 1.0 - player->y_pos) * deltaDistY;
+			sideDistY = (mapY + 1.0 - game->player.y_pos) * deltaDistY;
 		}
-		while (!wallHit)
+		while (hit == 0)
 		{
-			printf("JERE\n");
-			if(sideDistX < sideDistY)
+			if (sideDistX < sideDistY)
 			{
 				sideDistX += deltaDistX;
 				mapX += stepX;
-				// side = 0;
+				side = 0;
 			}
 			else
 			{
 				sideDistY += deltaDistY;
 				mapY += stepY;
-				// side = 1;
+				side = 1;
 			}
-			if(map->map[mapX][mapY] > 48)
-			{
-				wallHit = true;
-				printf("%c\n", map->map[mapX][mapY]);
-				printf("%d, %d\n", mapX, mapY);
-			}
+			if (game->map.map[mapX][mapY] == 49)
+				hit = 1;
 		}
+		if (side == 0)
+			perpWallDist = (sideDistX - deltaDistX);
+		else
+			perpWallDist = (sideDistY - deltaDistY);
+
+		int	lineHeight = (int)(WINDOW_HEIGHT / perpWallDist);
+
+		int	drawStart = -lineHeight / 2 + WINDOW_HEIGHT / 2;
+		if (drawStart < 0)
+			drawStart = 0;
+		int	drawEnd = lineHeight / 2 + WINDOW_HEIGHT / 2;
+		if (drawEnd > WINDOW_HEIGHT)
+			drawEnd = WINDOW_HEIGHT;
+
+		int	colour;
+		if (side == 0)
+			colour = 0x00E23434;
+		else
+			colour = 0x00C61D1D;
+		for (int y = drawStart; y < drawEnd; y++)
+			my_mlx_pixel_put(&game->raycast.data, x, y, colour);
 	}
-	printf("xit\n");
+	mlx_put_image_to_window(game->raycast.mlx_connection, game->raycast.mlx_window,
+		game->raycast.data.img, 0, 0);
+	return (EXIT_SUCCESS);
 }
