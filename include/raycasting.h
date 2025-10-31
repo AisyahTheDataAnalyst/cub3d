30f65003cee2f29476500115586ee06152310c21
Yohen Sheraun <ysheraun20@yahoo.com>
2025-10-31 15:22:34 +0800

    feat: added new types to structs

--- include/raycasting.h
+++ include/raycasting.h
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 17:21:49 by yosherau          #+#    #+#             */
-/*   Updated: 2025/10/24 17:21:54 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/10/27 15:40:35 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,32 @@
 # define RAYCASTING_H
 
 # include "parsing.h"
+// # include "cub3d.h"
 # include "../minilibx-linux/mlx.h"
 
 # define WINDOW_WIDTH 854
 # define WINDOW_HEIGHT 480
 
-typedef struct s_player	t_player;
-typedef struct s_map	t_map;
+// Why is this here?
+// typedef struct s_player	t_player;
+// typedef struct s_map	t_map;
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 typedef struct s_raycast
 {
 	void	*mlx_connection;
 	void	*mlx_window;
+	t_data	data;
 }	t_raycast;
 
+// Need to change this to elsewhere
 void	create_window(t_raycast *raycast);
 
 #endif
