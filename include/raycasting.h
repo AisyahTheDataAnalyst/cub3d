8fb30cbae91972bebe9086f441719573a3546096
Yohen Sheraun <ysheraun20@yahoo.com>
2025-11-02 16:20:39 +0800

    feature: added variables to hold time for FPS calculation

--- include/raycasting.h
+++ include/raycasting.h
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 17:21:49 by yosherau          #+#    #+#             */
-/*   Updated: 2025/10/27 15:40:35 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/11/01 15:20:59 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "parsing.h"
 // # include "cub3d.h"
 # include "../minilibx-linux/mlx.h"
+# include <sys/time.h>
 
 # define WINDOW_WIDTH 854
 # define WINDOW_HEIGHT 480
@@ -24,7 +25,8 @@
 // typedef struct s_player	t_player;
 // typedef struct s_map	t_map;
 
-typedef struct	s_data {
+typedef struct s_data
+{
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -37,6 +39,8 @@ typedef struct s_raycast
 	void	*mlx_connection;
 	void	*mlx_window;
 	t_data	data;
+	double	oldTime;
+	double	time;
 }	t_raycast;
 
 // Need to change this to elsewhere
