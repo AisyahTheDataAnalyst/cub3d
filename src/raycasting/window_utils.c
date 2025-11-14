06d27dba81e436e4faf182ecba2b7c1da6106169
Yohen Sheraun <yosherau@u91z02s02.42kl.edu.my>
2025-11-14 17:19:31 +0800

    refactor: added clearing of window and destruction

--- src/raycasting/window_utils.c
+++ src/raycasting/window_utils.c
@@ -6,22 +6,55 @@
 /*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 21:52:57 by yosherau          #+#    #+#             */
-/*   Updated: 2025/11/12 15:22:15 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/11/14 13:54:26 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycasting.h"
+#include "cub3d.h"
 
 void	create_window(t_raycast *raycast)
 {
 	raycast->mlx_connection = mlx_init();
 	raycast->mlx_window = mlx_new_window(raycast->mlx_connection,
-		WINDOW_WIDTH, WINDOW_HEIGHT, "cub3d");
+			WINDOW_WIDTH, WINDOW_HEIGHT, "cub3d");
 }
 
 void	create_window_image(t_raycast *raycast)
 {
-	raycast->data.img = mlx_new_image(raycast->mlx_connection, WINDOW_WIDTH, WINDOW_HEIGHT);
-	raycast->data.addr = mlx_get_data_addr(raycast->data.img, &raycast->data.bpp,
-		&raycast->data.ll, &raycast->data.endian);
+	raycast->data.img = mlx_new_image(raycast->mlx_connection,
+			WINDOW_WIDTH, WINDOW_HEIGHT);
+	raycast->data.addr = mlx_get_data_addr(raycast->data.img,
+			&raycast->data.bpp, &raycast->data.ll, &raycast->data.endian);
+}
+
+void	my_mlx_pixel_put(t_img_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->ll + x * (data->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	clear_window(t_game *game)
+{
+	int	x;
+	int	y;
+
+	y = -1;
+	while (++y < WINDOW_HEIGHT)
+	{
+		x = -1;
+		while (++x < WINDOW_WIDTH)
+			my_mlx_pixel_put(&game->raycast.data, x, y, 0x000000);
+	}
+}
+
+int	destroy_window(t_game *game)
+{
+	mlx_destroy_window(game->raycast.mlx_connection, game->raycast.mlx_window);
+	mlx_destroy_display(game->raycast.mlx_connection);
+	free(game->raycast.mlx_connection);
+	all_cleanup(game);
+	exit(EXIT_SUCCESS);
 }
