c018cec079efc2efa198b7f13302e7489159c0a1
Yohen Sheraun <yosherau@u91z02s02.42kl.edu.my>
2025-11-14 17:22:43 +0800

    refactor: added struct to reduce the amount of var declarations required

--- include/raycasting.h
+++ include/raycasting.h
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 17:21:49 by yosherau          #+#    #+#             */
-/*   Updated: 2025/11/12 15:23:47 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/11/14 16:10:31 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,45 @@
 # define RAYCASTING_H
 
 # include "parsing.h"
-// # include "cub3d.h"
 # include "../minilibx-linux/mlx.h"
 # include <sys/time.h>
 
+typedef struct s_game	t_game;
+
 # define WINDOW_WIDTH 854
 # define WINDOW_HEIGHT 480
+# define NUM_TEX 4
 
 // Why is this here?
 // typedef struct s_player	t_player;
 // typedef struct s_map	t_map;
+
+typedef struct s_ray
+{
+	double cameraX;
+	int		mapX;
+	int		mapY;
+	double	rayDirX;
+	double	rayDirY;
+	double	sideDistX;
+	double	sideDistY;
+	double	deltaDistX;
+	double	deltaDistY;
+	double	perpWallDist;
+	int		stepX;
+	int		stepY;
+	int		hit;
+	int		side;
+	int		lineHeight;
+	int		drawStart;
+	int		drawEnd;
+	double	wallX;
+	int		texX;
+	int		texY;
+	double	step;
+	double	texPos;
+	int		color;
+}	t_ray;
 
 typedef struct s_img_data
 {
@@ -32,7 +61,7 @@ typedef struct s_img_data
 	int		bpp;
 	int		ll;
 	int		endian;
-}				t_img_data;
+}	t_img_data;
 
 typedef struct s_assets
 {
@@ -55,5 +84,9 @@ typedef struct s_raycast
 // Need to change this to elsewhere
 void	create_window(t_raycast *raycast);
 void	create_window_image(t_raycast *raycast);
+void	clear_window(t_game *game);
+int		destroy_window(t_game *game);
+void	my_mlx_pixel_put(t_img_data *data, int x, int y, int color);
+void	draw_ceil_floor(t_game *game);
 
 #endif
