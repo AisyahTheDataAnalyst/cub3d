3803fd9b03753cd1a72236017c5eb49f0892033e
Yohen Sheraun <ysheraun20@yahoo.com>
2025-11-15 21:09:23 +0800

    refactor: added file to contain code relevant to movement only

---
+++ src/raycasting/movement_utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   movement_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/15 20:38:09 by yosherau          #+#    #+#             */
+/*   Updated: 2025/11/15 20:42:27 by yosherau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	move_forward(t_game *game)
+{
+	if (game->map.map[(int)game->player.y_pos][(int)(game->player.x_pos
+		+ game->player.dir_x * 0.11)] == '0')
+		game->player.x_pos += game->player.dir_x * 0.1;
+	if (game->map.map[(int)(game->player.y_pos
+			+ game->player.dir_y * 0.11)][(int)(game->player.x_pos)] == '0')
+		game->player.y_pos += game->player.dir_y * 0.1;
+}
+
+void	move_backward(t_game *game)
+{
+	if (game->map.map[(int)game->player.y_pos][(int)(game->player.x_pos
+		- game->player.dir_x * 0.11)] == '0')
+		game->player.x_pos -= game->player.dir_x * 0.1;
+	if (game->map.map[(int)(game->player.y_pos
+			- game->player.dir_y * 0.11)][(int)(game->player.x_pos)] == '0')
+		game->player.y_pos -= game->player.dir_y * 0.1;
+}
+
+void	move_left(t_game *game)
+{
+	if (game->map.map[(int)game->player.y_pos][(int)(game->player.x_pos
+		- game->player.plane_x * 0.11)] == '0')
+		game->player.x_pos -= game->player.plane_x * 0.1;
+	if (game->map.map[(int)(game->player.y_pos
+			- game->player.plane_y * 0.11)][(int)(game->player.x_pos)] == '0')
+		game->player.y_pos -= game->player.plane_y * 0.1;
+}
+
+void	move_right(t_game *game)
+{
+	if (game->map.map[(int)game->player.y_pos][(int)(game->player.x_pos
+		+ game->player.plane_x * 0.11)] == '0')
+		game->player.x_pos += game->player.plane_x * 0.1;
+	if (game->map.map[(int)(game->player.y_pos
+			+ game->player.plane_y * 0.11)][(int)(game->player.x_pos)] == '0')
+		game->player.y_pos += game->player.plane_y * 0.1;
+}
+
+void	rotate(t_game *game, double rot_speed)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = game->player.dir_x;
+	game->player.dir_x = game->player.dir_x
+		* cos(rot_speed) - game->player.dir_y * sin(rot_speed);
+	game->player.dir_y = old_dir_x * sin(rot_speed)
+		+ game->player.dir_y * cos(rot_speed);
+	old_plane_x = game->player.plane_x;
+	game->player.plane_x = game->player.plane_x
+		* cos(rot_speed) - game->player.plane_y * sin(rot_speed);
+	game->player.plane_y = old_plane_x * sin(rot_speed)
+		+ game->player.plane_y * cos(rot_speed);
+}
