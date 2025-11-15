a458c2d177cb5bf354decbcb80f2340bb52b5de1
Yohen Sheraun <ysheraun20@yahoo.com>
2025-11-15 21:06:57 +0800

    refactor: added struct to handle variable's for dda, included necessary function definitions

--- include/raycasting.h
+++ include/raycasting.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   raycasting.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 17:21:49 by yosherau          #+#    #+#             */
-/*   Updated: 2025/11/14 16:10:31 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/11/15 21:02:08 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@
 # include "parsing.h"
 # include "../minilibx-linux/mlx.h"
 # include <sys/time.h>
-
-typedef struct s_game	t_game;
+# include <math.h>
 
 # define WINDOW_WIDTH 854
 # define WINDOW_HEIGHT 480
 # define NUM_TEX 4
+
+typedef struct s_game	t_game;
 
 // Why is this here?
 // typedef struct s_player	t_player;
@@ -29,29 +30,30 @@ typedef struct s_game	t_game;
 
 typedef struct s_ray
 {
-	double cameraX;
-	int		mapX;
-	int		mapY;
-	double	rayDirX;
-	double	rayDirY;
-	double	sideDistX;
-	double	sideDistY;
-	double	deltaDistX;
-	double	deltaDistY;
-	double	perpWallDist;
-	int		stepX;
-	int		stepY;
+	double	camera_x;
+	double	ray_dir_x;
+	double	ray_dir_y;
+	int		map_x;
+	int		map_y;
+	double	side_dist_x;
+	double	side_dist_y;
+	double	delta_dist_x;
+	double	delta_dist_y;
+	double	perp_wall_dist;
+	int		step_x;
+	int		step_y;
 	int		hit;
 	int		side;
-	int		lineHeight;
-	int		drawStart;
-	int		drawEnd;
-	double	wallX;
-	int		texX;
-	int		texY;
+	int		line_height;
+	int		draw_start;
+	int		draw_end;
+	double	wall_x;
+	int		tex_x;
 	double	step;
-	double	texPos;
+	double	tex_pos;
 	int		color;
+	int		tex_y;
+
 }	t_ray;
 
 typedef struct s_img_data
@@ -61,7 +63,7 @@ typedef struct s_img_data
 	int		bpp;
 	int		ll;
 	int		endian;
-}	t_img_data;
+}				t_img_data;
 
 typedef struct s_assets
 {
@@ -76,17 +78,32 @@ typedef struct s_raycast
 	void		*mlx_connection;
 	void		*mlx_window;
 	t_img_data	data;
-	double		oldTime;
+	double		old_time;
 	double		time;
 	t_assets	assets;
 }	t_raycast;
 
 // Need to change this to elsewhere
+void	free_resources(t_game *game);
 void	create_window(t_raycast *raycast);
 void	create_window_image(t_raycast *raycast);
 void	clear_window(t_game *game);
-int		destroy_window(t_game *game);
+void	sprite_loader(t_game *game);
 void	my_mlx_pixel_put(t_img_data *data, int x, int y, int color);
-void	draw_ceil_floor(t_game *game);
+int		destroy_window(t_game *game);
+void	move_forward(t_game *game);
+void	move_backward(t_game *game);
+void	move_left(t_game *game);
+void	move_right(t_game *game);
+void	rotate(t_game *game, double rot_speed);
+int		handle_input(int keycode, t_game *game);
+void	my_mlx_pixel_put(t_img_data *data, int x, int y, int color);
+int		get_pixel(t_img_data *data, int x, int y);
+void	init_step_and_side(t_game *game, t_ray *ray);
+void	fill_ceil_floor(t_game *game);
+void	init_ray(t_game *game, t_ray *ray, int x);
+void	dda(t_game *game, t_ray *ray);
+void	calculate_wall(t_game *game, t_ray *ray);
+void	draw_walls(t_game *game, t_ray *ray, int x);
 
 #endif
