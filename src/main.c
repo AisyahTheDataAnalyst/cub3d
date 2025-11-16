e6e41d074a21b467f1b5a79f59c0cd45fd9d31be
Yohen Sheraun <ysheraun20@yahoo.com>
2025-11-16 16:43:10 +0800

    feat: changed input handler to set key presses to true or false to allow for smoother movement

--- src/main.c
+++ src/main.c
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aimokhta <aimokhta@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 15:26:25 by aimokhta          #+#    #+#             */
-/*   Updated: 2025/11/16 14:30:03 by aimokhta         ###   ########.fr       */
+/*   Updated: 2025/11/16 16:38:01 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	raycast_init(t_game *game)
+static void	game_init(t_game *game)
 {
+	ft_bzero(game, sizeof(t_game));
 	create_window(&game->raycast);
 	create_window_image(&game->raycast);
 }
@@ -34,24 +35,39 @@ static void	raycast_init(t_game *game)
 // 	printf("\n");
 // }
 
-int	handle_input(int keycode, t_game *game)
+int	handle_press(int keycode, t_game *game)
 {
-	const double	rot_speed = 0.05;
-
-	if (keycode == 65307)
+	if (keycode == ESC_KEY)
 		destroy_window(game);
-	if (keycode == 119)
-		move_forward(game);
-	if (keycode == 115)
-		move_backward(game);
-	if (keycode == 97)
-		move_left(game);
-	if (keycode == 100)
-		move_right(game);
-	if (keycode == 65363)
-		rotate(game, rot_speed);
-	if (keycode == 65361)
-		rotate(game, -rot_speed);
+	if (keycode == W_KEY)
+		game->keys.w = true;
+	if (keycode == S_KEY)
+		game->keys.s = true;
+	if (keycode == A_KEY)
+		game->keys.a = true;
+	if (keycode == D_KEY)
+		game->keys.d = true;
+	if (keycode == LEFT_KEY)
+		game->keys.left = true;
+	if (keycode == RIGHT_KEY)
+		game->keys.right = true;
+	return (EXIT_SUCCESS);
+}
+
+int	handle_release(int keycode, t_game *game)
+{
+	if (keycode == W_KEY)
+		game->keys.w = false;
+	if (keycode == S_KEY)
+		game->keys.s = false;
+	if (keycode == A_KEY)
+		game->keys.a = false;
+	if (keycode == D_KEY)
+		game->keys.d = false;
+	if (keycode == LEFT_KEY)
+		game->keys.left = false;
+	if (keycode == RIGHT_KEY)
+		game->keys.right = false;
 	return (EXIT_SUCCESS);
 }
 
@@ -63,6 +79,7 @@ int	raycasting(t_game *game)
 	x = -1;
 	clear_window(game);
 	fill_ceil_floor(game);
+	update_player(game);
 	while (++x < WINDOW_WIDTH)
 	{
 		init_ray(game, &ray, x);
@@ -75,19 +92,20 @@ int	raycasting(t_game *game)
 	return (EXIT_SUCCESS);
 }
 
-	// print_last_look_map(&game.map);
+// print_last_look_map(&game.map);
 int	main(int ac, char **av)
 {
 	t_game		game;
 
 	if (ac != 2)
 		return (ft_putstr_fd("Usage: ./program maps/map\n", 2), 1);
-	ft_bzero(&game, sizeof(t_game));
+	game_init(&game);
 	if (parsing(&game.parse, &game.player, &game.map, av) == 1)
 		return (parsing_cleanup(&game.parse, &game.map), 1);
-	raycast_init(&game);
+	ft_putendl_fd("Success parsing!", 1);
 	sprite_loader(&game);
-	mlx_hook(game.raycast.mlx_window, 2, 1L << 0, handle_input, &game);
+	mlx_hook(game.raycast.mlx_window, 2, 1L << 0, handle_press, &game);
+	mlx_hook(game.raycast.mlx_window, 3, 1L << 1, handle_release, &game);
 	mlx_hook(game.raycast.mlx_window, 17, 1L << 17, destroy_window, &game);
 	mlx_loop_hook(game.raycast.mlx_connection, &raycasting, &game);
 	mlx_loop(game.raycast.mlx_connection);
