61d9b4e8ad81533d876b28ae8dbdce0a8ea78032
Yohen Sheraun <ysheraun20@yahoo.com>
2025-11-16 16:42:34 +0800

    feat: added function to make movement more smoother

--- src/raycasting/draw_utils.c
+++ src/raycasting/draw_utils.c
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 20:47:15 by yosherau          #+#    #+#             */
-/*   Updated: 2025/11/15 20:47:51 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/11/16 16:40:43 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,24 @@ int	get_pixel(t_img_data *data, int x, int y)
 		y = 64;
 	pixel = data->addr + (y * data->ll + x * (data->bpp / 8));
 	return (*(unsigned int *)pixel);
+}
+
+void	update_player(t_game *game)
+{
+	const double	rot_speed = 0.02;
+
+	if (game->keys.w)
+		move_forward(game);
+	if (game->keys.s)
+		move_backward(game);
+	if (game->keys.a)
+		move_left(game);
+	if (game->keys.d)
+		move_right(game);
+	if (game->keys.left)
+		rotate(game, rot_speed);
+	if (game->keys.right)
+		rotate(game, -rot_speed);
 }
 
 void	fill_ceil_floor(t_game *game)
